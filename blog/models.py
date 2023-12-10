3f6a7c2f5f634047afdefaade02fed136d453ab7
Ayush Sharma <ayush@cyboforge.in>
2023-12-10 21:41:22 +0530

    added post model to blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+  title = models.CharField(max_length=255, blank=False, null=False)
+  content = models.TextField(blank=False, null=False)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  created_by = models.ForeignKey('user.User', related_name='blog_post', on_delete=models.CASCADE, blank=False, null=False)
+
+  def __str__(self):
+    return self.title
